324dd68d25635af5a7c1117e94e4b956c475a6b9
pro <lehoangthai2312@gmail.com>
2022-06-27 19:26:47 +0700

    Thai push

---
+++ TGDD/src/test/java/com/project/TGDD/TgddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TGDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
