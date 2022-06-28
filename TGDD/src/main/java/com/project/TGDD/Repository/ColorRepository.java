cd4f1f54c948f44366455b76efb21032a4257abc
Thai LH <Lehoangthai2312@gmail.com>
2022-06-28 16:48:37 +0700

    Thai push 28/06 moi nua

--- TGDD/src/main/java/com/project/TGDD/Repository/ColorRepository.java
+++ TGDD/src/main/java/com/project/TGDD/Repository/ColorRepository.java
@@ -1,8 +1,9 @@
 package com.project.TGDD.Repository;
 
+import com.project.TGDD.Model.Color;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ColorRepository extends CrudRepository<ColorRepository, Integer> {
+public interface ColorRepository extends CrudRepository<Color, Integer> {
 }
