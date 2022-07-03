4c9bc3fe8c7a5eb30d285ffba17e91f86e535d69
Thai LH <Lehoangthai2312@gmail.com>
2022-07-03 16:32:41 +0700

    Thai push fix addProduct

--- TGDD/src/main/java/com/project/TGDD/Controller/ProductController.java
+++ TGDD/src/main/java/com/project/TGDD/Controller/ProductController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -29,11 +31,12 @@ public class ProductController {
         model.addAttribute("listManufacturer", listManufacturer);
         List<Rom> listRom = productService.getAllRom();
         model.addAttribute("listRom", listRom);
+        model.addAttribute("Manufacturer", new Manufacturer());
         return "AddProduct";
     }
 
     @GetMapping("/Product/AddManu")
-    public String AddManufacturer(Manufacturer manu,RedirectAttributes re) {
+    public String AddManufacturer(Manufacturer manu, RedirectAttributes re) {
         List<Manufacturer> listManufacturer = productService.getAllManufacturer();
         if(manu.getManufacturerName().isEmpty()){
             re.addFlashAttribute("ErrorNull", "Manufacturer name is null");
